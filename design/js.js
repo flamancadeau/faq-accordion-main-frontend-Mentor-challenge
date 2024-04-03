806d67c0e10479d586856c969df24558d971a149
flamancadeau <irakozeflamanc@gmail.com>
2024-04-04 00:33:43 +0200

    accordion-main-frontend-Mentor-challenge

---
+++ design/js.js
@@ -0,0 +1,64 @@
+function toggleContent() {
+  let buttonPlus = document.getElementById("button-plus");
+  let buttonMinus = document.getElementById("button-minus");
+  let content = document.getElementById("content");
+
+  if (content.style.display === "none") {
+    content.style.display = "block";
+    buttonPlus.style.display = "none";
+    buttonMinus.style.display = "inline-block"; 
+  } else {
+    content.style.display = "none";
+    buttonPlus.style.display = "inline-block";
+    buttonMinus.style.display = "none";
+  }
+}
+
+
+function toggleContent_1() {
+  let buttonPlus_1 = document.getElementById("button-plus_1");
+  let buttonMinus_1 = document.getElementById("button-minus_1");
+  let content_1 = document.getElementById("content_1");
+
+  if (content_1.style.display === "none") {
+    content_1.style.display = "block";
+    buttonPlus_1.style.display = "none";
+    buttonMinus_1.style.display = "inline-block"; 
+  } else {
+    content_1.style.display = "none";
+    buttonPlus_1.style.display = "inline-block";
+    buttonMinus_1.style.display = "none";
+  }
+}
+
+function toggleContent_2() {
+  let buttonPlus_2 = document.getElementById("button-plus_2");
+  let buttonMinus_2 = document.getElementById("button-minus_2");
+  let content_2 = document.getElementById("content_2");
+
+  if (content_2.style.display === "none") {
+    content_2.style.display = "block";
+    buttonPlus_2.style.display = "none";
+    buttonMinus_2.style.display = "inline-block"; 
+  } else {
+    content_2.style.display = "none";
+    buttonPlus_2.style.display = "inline-block";
+    buttonMinus_2.style.display = "none";
+  }
+}
+
+function toggleContent_3() {
+  let buttonPlus_3 = document.getElementById("button-plus_3");
+  let buttonMinus_3 = document.getElementById("button-minus_3");
+  let content_3 = document.getElementById("content_3");
+
+  if (content_3.style.display === "none") {
+    content_3.style.display = "block";
+    buttonPlus_3.style.display = "none";
+    buttonMinus_3.style.display = "inline-block"; 
+  } else {
+    content_3.style.display = "none";
+    buttonPlus_3.style.display = "inline-block";
+    buttonMinus_3.style.display = "none";
+  }
+}
